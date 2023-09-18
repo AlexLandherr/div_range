f226a8d5e58b15115d860bc8c5b7d331d3350597
Alex Landherr <alex.landherr@gmail.com>
2023-09-18 09:54:11 +0000

    Init commit.

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+2520 is the smallest number that can be divided by each of the numbers 1 through 10 without a remainder.
+What is the smallest number that can be divided by each of the numbers from 1 through 20 without a remainder?
+
+So the number must be evenly divisible by all numbers 1 - 20.
+*/
+
+int main() {
+
+    return 0;
+}
